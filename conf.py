0421d90bcea25fbffe67f652dc095627fbc26711
Richard Darst <rkd@zgib.net>
2020-06-26 15:14:57 +0300

    sphinx: link index.md<-README.md for dirhtml web building

--- conf.py
+++ conf.py
@@ -37,7 +37,9 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'venv']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'venv',
+                    'README.md',  # index.md linked to this, for web serving
+                    ]
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -52,7 +54,7 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-master_doc = 'README'
+master_doc = 'index'
 
 html_context = {'display_github': True,
                 'github_user': 'coderefinery',
