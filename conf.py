9de38f5d0d439b99413c6c069d67e3356656ff46
Richard Darst <rkd@zgib.net>
2020-07-31 01:55:21 +0300

    conf: fix path to the favicon

--- conf.py
+++ conf.py
@@ -53,7 +53,7 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-html_favicon = 'coderefinery.ico'
+html_favicon = '_static/coderefinery.ico'
 
 master_doc = 'index'
 
